88d82e914c76b5e888916660479b8ec99ee77a97
MaartinV2 <maartin.venter@gmail.com>
2021-09-08 14:49:15 +0200

    Swagger Setup and Working

--- AccountSystem/AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
+++ AccountSystem/AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
@@ -1,4 +1,15 @@
 package za.ac.nwu.ac.web.sb.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import  za.ac.nwu.ac.domain.service.GeneralResponse;
+
+@RestController
 public class AccountTypeController {
+
+    @GetMapping("/all")
+    public GeneralResponse<String> getAll(){
+        return new GeneralResponse<>(true,"hi");
+    }
+
 }
